80e5afa0c557080da75d10df07493eb942eafbef
Peter Zijlstra <peterz@infradead.org>
2020-02-19 09:46:47 +0100

    hardirq/nmi: Allow nested nmi_enter()
    Since there are already a number of sites (ARM64, PowerPC) that effectively
    nest nmi_enter(), make the primitive support this before adding even more.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Marc Zyngier <maz@kernel.org>
    Acked-by: Will Deacon <will@kernel.org>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Link: https://lkml.kernel.org/r/20200505134100.864179229@linutronix.de

--- arch/powerpc/kernel/traps.c
+++ arch/powerpc/kernel/traps.c
@@ -385,8 +385,10 @@ void system_reset_exception(struct pt_regs *regs)
 	 * is determined by RI and in_nmi
 	 */
 	bool nested = in_nmi();
-	if (!nested)
-		nmi_enter();
+	unsigned long hsrr0, hsrr1;
+	bool saved_hsrrs = false;
+
+	nmi_enter();
 
 	__this_cpu_inc(irq_stat.sreset_irqs);
 
@@ -439,8 +441,7 @@ out:
 		die("Unrecoverable System Reset", regs, SIGABRT);
 	}
 
-	if (!nested)
-		nmi_exit();
+	nmi_exit();
 
 	/* What should we do here? We could issue a shutdown or hard reset. */
 }
@@ -741,9 +742,8 @@ int machine_check_generic(struct pt_regs *regs)
 void machine_check_exception(struct pt_regs *regs)
 {
 	int recover = 0;
-	bool nested = in_nmi();
-	if (!nested)
-		nmi_enter();
+
+	nmi_enter();
 
 	/* 64s accounts the mce in machine_check_early when in HVMODE */
 	if (!IS_ENABLED(CONFIG_PPC_BOOK3S_64) || !cpu_has_feature(CPU_FTR_HVMODE))
@@ -771,8 +771,7 @@ void machine_check_exception(struct pt_regs *regs)
 	if (check_io_access(regs))
 		goto bail;
 
-	if (!nested)
-		nmi_exit();
+	nmi_exit();
 
 	die("Machine check", regs, SIGBUS);
 
@@ -783,8 +782,7 @@ void machine_check_exception(struct pt_regs *regs)
 	return;
 
 bail:
-	if (!nested)
-		nmi_exit();
+	nmi_exit();
 }
 
 void SMIException(struct pt_regs *regs)
