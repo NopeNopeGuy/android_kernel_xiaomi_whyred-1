d3d2dc61cd6bd38b83b15f857006a7dd9adeb708
Reinazhard <reinazhard@gmail.com>
2022-03-26 16:28:15 +0100

    fix
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- fs/ext4/readpage.c
+++ fs/ext4/readpage.c
@@ -71,6 +71,7 @@ static void __read_end_io(struct bio *bio)
 {
 	struct page *page;
 	struct bio_vec *bv;
+	struct bvec_iter_all iter_all;
 	int i;
 
 	bio_for_each_segment_all(bv, bio, i, iter_all) {
@@ -172,10 +173,6 @@ ext4_trace_read_completion(struct bio *bio)
  */
 static void mpage_end_io(struct bio *bio)
 {
-	struct bio_vec *bv;
-	int i;
-	struct bvec_iter_all iter_all;
-
 	if (trace_android_fs_dataread_start_enabled())
 		ext4_trace_read_completion(bio);
 
