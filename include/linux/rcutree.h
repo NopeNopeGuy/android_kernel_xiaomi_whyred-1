653b873527be2b0af96cb244d455beef89e5a9c6
Paul E. McKenney <paulmck@kernel.org>
2022-04-15 10:55:42 -0700

    rcu: Add polled expedited grace-period primitives
    This commit adds expedited grace-period functionality to RCU's polled
    grace-period API, adding start_poll_synchronize_rcu_expedited() and
    cond_synchronize_rcu_expedited(), which are similar to the existing
    start_poll_synchronize_rcu() and cond_synchronize_rcu() functions,
    respectively.
    
    Note that although start_poll_synchronize_rcu_expedited() can be invoked
    very early, the resulting expedited grace periods are not guaranteed
    to start until after workqueues are fully initialized.  On the other
    hand, both synchronize_rcu() and synchronize_rcu_expedited() can also
    be invoked very early, and the resulting grace periods will be taken
    into account as they occur.
    
    [ paulmck: Apply feedback from Neeraj Upadhyay. ]
    
    Link: https://lore.kernel.org/all/20220121142454.1994916-1-bfoster@redhat.com/
    Link: https://docs.google.com/document/d/1RNKWW9jQyfjxw2E8dsXVTdvZYh0HnYeSHDKog9jhdN8/edit?usp=sharing
    Cc: Brian Foster <bfoster@redhat.com>
    Cc: Dave Chinner <david@fromorbit.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Ian Kent <raven@themaw.net>
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>

--- include/linux/rcutree.h
+++ include/linux/rcutree.h
@@ -51,6 +51,8 @@ void kvfree_call_rcu(struct rcu_head *head, rcu_callback_t func);
 void rcu_barrier(void);
 bool rcu_eqs_special_set(int cpu);
 void kfree_rcu_scheduler_running(void);
+unsigned long start_poll_synchronize_rcu_expedited(void);
+void cond_synchronize_rcu_expedited(unsigned long oldstate);
 unsigned long get_state_synchronize_rcu(void);
 unsigned long start_poll_synchronize_rcu(void);
 bool poll_state_synchronize_rcu(unsigned long oldstate);
