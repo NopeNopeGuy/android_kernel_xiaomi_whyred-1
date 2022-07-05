7a0d949672e962ff1e7ce18e6fa62649d52fcb01
Reinazhard <reinazhard@gmail.com>
2022-07-05 04:37:36 +0200

    Revert "FROMGIT: block/bfq: fix ifdef for CONFIG_BFQ_GROUP_IOSCHED=y"
    This reverts commit 5fb28ef4081b94f05b71b45f737a3dac1de2a4aa.

--- block/bfq-wf2q.c
+++ block/bfq-wf2q.c
@@ -1018,7 +1018,7 @@ static void __bfq_activate_entity(struct bfq_entity *entity,
 		entity->on_st = true;
 	}
 
-#ifdef CONFIG_BFQ_GROUP_IOSCHED
+#ifdef BFQ_GROUP_IOSCHED_ENABLED
 	if (!bfq_entity_to_bfqq(entity)) { /* bfq_group */
 		struct bfq_group *bfqg =
 			container_of(entity, struct bfq_group, entity);
