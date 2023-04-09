PradyX/unified_kernel_sdm660

drivers: power: import minimal xiaomi changes
Signed-off-by: OdSazib <odsazib@gmail.com>
Change-Id: Id9353e51f71ced5433d41605038680fe0bdc3b9a

drivers/of/of_batterydata.c
@@ -370,7 +370,20 @@ struct device_node *of_batterydata_get_best_profile(
 	}
 
 	if (best_node == NULL) {
+#ifdef CONFIG_XIAOMI
+		pr_info("sunxing detect No battery data configed,add default\n");
+		for_each_child_of_node(batterydata_container_node, node) {
+			rc = of_property_read_string(node, "qcom,battery-type", &battery_type);
+			if (!rc && strcmp(battery_type, "unknown-default") == 0) {
+				best_node = node;
+				break;
+			}
+		}
+		if (best_node)
+			pr_info("use unknown battery data\n");
+#else
 		pr_err("No battery data found\n");
+#endif
 		return best_node;
 	}
 

drivers/power/supply/qcom/battery.c
@@ -1206,7 +1206,11 @@ static bool is_batt_available(struct pl_data *chip)
 	return true;
 }
 
+#ifdef CONFIG_XIAOMI
+#define PARALLEL_FLOAT_VOLTAGE_DELTA_UV 100000
+#else
 #define PARALLEL_FLOAT_VOLTAGE_DELTA_UV 50000
+#endif
 static int pl_fv_vote_callback(struct votable *votable, void *data,
 			int fv_uv, const char *client)
 {
@@ -1269,6 +1273,11 @@ static int pl_fv_vote_callback(struct votable *votable, void *data,
 
 #define ICL_STEP_UA	25000
 #define PL_DELAY_MS     3000
+#ifdef CONFIG_XIAOMI
+#define ICL_MAX_UA	1300000
+#else
+#define ICL_MAX_UA	1400000
+#endif
 static int usb_icl_vote_callback(struct votable *votable, void *data,
 			int icl_ua, const char *client)
 {
@@ -1291,7 +1300,7 @@ static int usb_icl_vote_callback(struct votable *votable, void *data,
 	vote(chip->pl_disable_votable, ICL_CHANGE_VOTER, true, 0);
 
 	/*
-	 * if (ICL < 1400)
+	 * if (ICL < ICL_MAX_UA)
 	 *	disable parallel charger using USBIN_I_VOTER
 	 * else
 	 *	instead of re-enabling here rely on status_changed_work
@@ -1299,7 +1308,7 @@ static int usb_icl_vote_callback(struct votable *votable, void *data,
 	 *	unvote USBIN_I_VOTER) the status_changed_work enables
 	 *	USBIN_I_VOTER based on settled current.
 	 */
-	if (icl_ua <= 1400000)
+	if (icl_ua <= ICL_MAX_UA)
 		vote(chip->pl_enable_votable_indirect, USBIN_I_VOTER, false, 0);
 	else
 		schedule_delayed_work(&chip->status_change_work,
@@ -1769,10 +1778,10 @@ static void handle_settled_icl_change(struct pl_data *chip)
 	}
 	main_limited = pval.intval;
 
-	if ((main_limited && (main_settled_ua + chip->pl_settled_ua) < 1400000)
+	if ((main_limited && (main_settled_ua + chip->pl_settled_ua) < ICL_MAX_UA)
 			|| (main_settled_ua == 0)
 			|| ((total_current_ua >= 0) &&
-				(total_current_ua <= 1400000)))
+				(total_current_ua <= ICL_MAX_UA)))
 		vote(chip->pl_enable_votable_indirect, USBIN_I_VOTER, false, 0);
 	else
 		vote(chip->pl_enable_votable_indirect, USBIN_I_VOTER, true, 0);

drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -615,6 +615,39 @@ static int fg_get_battery_temp(struct fg_dev *fg, int *val)
 
 	/* Value is in Kelvin; Convert it to deciDegC */
 	temp = (temp - 273) * 10;
+#ifdef CONFIG_XIAOMI
+	if (temp < -80) {
+		switch (temp) {
+			case -90:
+				temp = -110;
+				break;
+			case -100:
+				temp = -120;
+				break;
+			case -110:
+				temp = -130;
+				break;
+			case -120:
+				temp = -150;
+				break;
+			case -130:
+				temp = -170;
+				break;
+			case -140:
+				temp = -190;
+				break;
+			case -150:
+				temp = -200;
+				break;
+			case -160:
+				temp = -210;
+				break;
+			default:
+				temp -= 50;
+				break;
+		};
+	}
+#endif
 	*val = temp;
 	return 0;
 }
@@ -1854,10 +1887,17 @@ static int fg_adjust_recharge_voltage(struct fg_dev *fg)
 
 	recharge_volt_mv = chip->dt.recharge_volt_thr_mv;
 
+#ifdef CONFIG_XIAOMI
+	if (fg->health == POWER_SUPPLY_HEALTH_WARM)
+		recharge_volt_mv = 4050;
+	if (fg->health == POWER_SUPPLY_HEALTH_COOL)
+		recharge_volt_mv = 4280;
+#else
 	/* Lower the recharge voltage in soft JEITA */
 	if (fg->health == POWER_SUPPLY_HEALTH_WARM ||
 			fg->health == POWER_SUPPLY_HEALTH_COOL)
 		recharge_volt_mv -= 200;
+#endif
 
 	rc = fg_set_recharge_voltage(fg, recharge_volt_mv);
 	if (rc < 0) {
@@ -4182,6 +4222,9 @@ static int fg_hw_init(struct fg_dev *fg)
 	if (chip->dt.delta_soc_thr > 0 && chip->dt.delta_soc_thr < 100) {
 		fg_encode(fg->sp, FG_SRAM_DELTA_MSOC_THR,
 			chip->dt.delta_soc_thr, buf);
+#ifdef CONFIG_XIAOMI
+		buf[0] = 0x8;
+#endif
 		rc = fg_sram_write(fg,
 				fg->sp[FG_SRAM_DELTA_MSOC_THR].addr_word,
 				fg->sp[FG_SRAM_DELTA_MSOC_THR].addr_byte,
@@ -4416,6 +4459,14 @@ static int fg_hw_init(struct fg_dev *fg)
 			return rc;
 		}
 	}
+
+#ifdef CONFIG_XIAOMI
+	buf[0] = 0x33;
+	buf[1] = 0x3;
+	rc = fg_sram_write(fg, 4, 0, buf, 2, FG_IMA_DEFAULT);
+	if (rc < 0)
+		pr_err("Error in configuring Sram,rc = %d\n",rc);
+#endif
 
 	return 0;
 }
@@ -5076,7 +5127,11 @@ static int fg_parse_dt(struct fg_gen3_chip *chip)
 	if (rc < 0)
 		chip->dt.sys_term_curr_ma = DEFAULT_SYS_TERM_CURR_MA;
 	else
+#ifdef CONFIG_XIAOMI
+		chip->dt.sys_term_curr_ma = -temp;
+#else
 		chip->dt.sys_term_curr_ma = temp;
+#endif
 
 	rc = of_property_read_u32(node, "qcom,fg-chg-term-base-current", &temp);
 	if (rc < 0)

drivers/power/supply/qcom/qpnp-smb2.c
@@ -1152,7 +1152,11 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 					      FG_ESR_VOTER);
 		break;
 	case POWER_SUPPLY_PROP_TECHNOLOGY:
+#ifdef CONFIG_XIAOMI
+		val->intval = POWER_SUPPLY_TECHNOLOGY_LIPO;
+#else
 		val->intval = POWER_SUPPLY_TECHNOLOGY_LION;
+#endif
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_DONE:
 		rc = smblib_get_prop_batt_charge_done(chg, val);
@@ -1184,12 +1188,16 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_VOLTAGE_NOW:
 	case POWER_SUPPLY_PROP_TEMP:
 	case POWER_SUPPLY_PROP_TIME_TO_FULL_NOW:
+#ifndef CONFIG_XIAOMI
 		rc = smblib_get_prop_from_bms(chg, psp, val);
 		break;
+#endif
 	case POWER_SUPPLY_PROP_CURRENT_NOW:
 		rc = smblib_get_prop_from_bms(chg, psp, val);
+#ifndef CONFIG_XIAOMI
 		if (!rc)
 			val->intval *= (-1);
+#endif
 		break;
 	case POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE:
 		val->intval = chg->fcc_stepper_enable;
@@ -1711,6 +1719,23 @@ static int smb2_init_hw(struct smb2 *chip)
 			true, 0);
 	vote(chg->pd_disallowed_votable_indirect, PD_NOT_SUPPORTED_VOTER,
 			chip->dt.no_pd, 0);
+
+#ifdef CONFIG_XIAOMI
+	/* Operate the QC2.0 in 5V/9V mode i.e. Disable 12V */
+	rc = smblib_masked_write(chg, HVDCP_PULSE_COUNT_MAX_REG,
+								PULSE_COUNT_QC2P0_12V | PULSE_COUNT_QC2P0_9V, PULSE_COUNT_QC2P0_9V);
+	if (rc < 0) {
+		dev_err(chg->dev, "Couldn't configure QC2.0 to 9V rc=%d\n", rc);
+		return rc;
+	}
+	/* Operate the QC3.0 to limit vbus to 6.6v*/
+	rc = smblib_masked_write(chg, HVDCP_PULSE_COUNT_MAX_REG, PULSE_COUNT_QC3P0_MASK, 0x8);
+	if (rc < 0) {
+		dev_err(chg->dev, "Couldn't configure QC3.0 to 6.6V rc=%d\n", rc);
+		return rc;
+	}
+#endif
+
 	/*
 	 * AICL configuration:
 	 * start from min and AICL ADC disable
@@ -1876,6 +1901,10 @@ static int smb2_init_hw(struct smb2 *chip)
 		return rc;
 	}
 
+#ifdef CONFIG_XIAOMI
+	rc = vote(chg->chg_disable_votable, DEFAULT_VOTER, true, 0);
+#endif
+
 	switch (chip->dt.chg_inhibit_thr_mv) {
 	case 50:
 		rc = smblib_masked_write(chg, CHARGE_INHIBIT_THRESHOLD_CFG_REG,
@@ -1903,6 +1932,10 @@ static int smb2_init_hw(struct smb2 *chip)
 	default:
 		break;
 	}
+
+#ifdef CONFIG_XIAOMI
+	rc = vote(chg->chg_disable_votable, DEFAULT_VOTER, false, 0);
+#endif
 
 	if (rc < 0) {
 		dev_err(chg->dev, "Couldn't configure charge inhibit threshold rc=%d\n",

drivers/power/supply/qcom/smb1351-charger.c
@@ -533,6 +533,10 @@ static int pre_chg_current[] = {
 	200, 300, 400, 500, 600, 700,
 };
 
+#ifdef CONFIG_XIAOMI
+bool is_global_version = false;
+#endif
+
 static int smb1351_read_reg(struct smb1351_charger *chip, int reg, u8 *val)
 {
 	s32 ret;
@@ -734,7 +738,9 @@ static int smb1351_fastchg_current_set(struct smb1351_charger *chip,
 		(fastchg_current > SMB1351_CHG_FAST_MAX_MA)) {
 		pr_err("bad pre_fastchg current mA=%d asked to set\n",
 					fastchg_current);
+#ifndef CONFIG_XIAOMI
 		return -EINVAL;
+#endif
 	}
 
 	/*
@@ -1567,7 +1573,9 @@ static int smb1351_parallel_set_chg_suspend(struct smb1351_charger *chip,
 	if (chip->parallel_charger_suspended == suspend) {
 		pr_debug("Skip same state request suspended = %d suspend=%d\n",
 				chip->parallel_charger_suspended, !suspend);
+#ifndef CONFIG_XIAOMI
 		return 0;
+#endif
 	}
 
 	if (!suspend) {
@@ -1661,6 +1669,18 @@ static int smb1351_parallel_set_chg_suspend(struct smb1351_charger *chip,
 		}
 		chip->parallel_charger_suspended = false;
 	} else {
+#ifdef CONFIG_XIAOMI
+		smb1351_enable_volatile_writes(chip);
+		/* control USB suspend via command bits */
+		rc = smb1351_masked_write(chip, VARIOUS_FUNC_REG,
+					APSD_EN_BIT | SUSPEND_MODE_CTRL_BIT,
+						SUSPEND_MODE_CTRL_BY_I2C);
+		if (rc) {
+			pr_err("Couldn't set USB suspend rc=%d\n", rc);
+			return rc;
+		}
+#endif
+
 		rc = smb1351_usb_suspend(chip, CURRENT, true);
 		if (rc)
 			pr_debug("failed to suspend rc=%d\n", rc);
@@ -2727,6 +2747,16 @@ static int is_parallel_charger(struct i2c_client *client)
 	return of_property_read_bool(node, "qcom,parallel-charger");
 }
 
+#ifdef CONFIG_XIAOMI
+static int __init hwc_setup(char *s)
+{
+	is_global_version = strcmp(s, "Global") != 0;
+	return 1;
+}
+
+__setup("androidboot.hwc=", hwc_setup);
+#endif
+
 static int create_debugfs_entries(struct smb1351_charger *chip)
 {
 	struct dentry *ent;
@@ -2944,6 +2974,12 @@ static int smb1351_parallel_charger_probe(struct i2c_client *client,
 	struct device_node *node = client->dev.of_node;
 	struct power_supply_config parallel_psy_cfg = {};
 
+#ifdef CONFIG_XIAOMI
+	if (is_global_version) {
+		return -ENODEV;
+	}
+#endif
+
 	chip = devm_kzalloc(&client->dev, sizeof(*chip), GFP_KERNEL);
 	if (!chip)
 		return -ENOMEM;
@@ -3117,7 +3153,29 @@ static struct i2c_driver smb1351_charger_driver = {
 	.id_table	= smb1351_charger_id,
 };
 
+#ifdef CONFIG_XIAOMI
+static int __init smb1351_charger_init(void)
+{
+	struct power_supply *pl_psy = power_supply_get_by_name("parallel");
+
+	if (pl_psy) {
+		pr_info("Another parallel driver has been registered\n");
+		return -ENOENT;
+	}
+
+	return i2c_add_driver(&smb1351_charger_driver);
+}
+
+static void __exit smb1351_charger_exit(void)
+{
+	i2c_del_driver(&smb1351_charger_driver);
+}
+
+late_initcall(smb1351_charger_init);
+module_exit(smb1351_charger_exit);
+#else
 module_i2c_driver(smb1351_charger_driver);
+#endif
 
 MODULE_DESCRIPTION("smb1351 Charger");
 MODULE_LICENSE("GPL v2");

NopeNopeGuy/android_kernel_xiaomi_whyred-1

drivers/of/of_batterydata.c
@@ -370,7 +370,20 @@ struct device_node *of_batterydata_get_best_profile(
 	}
 
 	if (best_node == NULL) {
+#ifdef CONFIG_MACH_XIAOMI_SDM660
+		pr_info("sunxing detect No battery data configed,add default\n");
+		for_each_child_of_node(batterydata_container_node, node) {
+			rc = of_property_read_string(node, "qcom,battery-type", &battery_type);
+			if (!rc && strcmp(battery_type, "unknown-default") == 0) {
+				best_node = node;
+				break;
+			}
+		}
+		if (best_node)
+			pr_info("use unknown battery data\n");
+#else
 		pr_err("No battery data found\n");
+#endif
 		return best_node;
 	}
 

drivers/power/supply/qcom/battery.c
@@ -1206,7 +1206,11 @@ static bool is_batt_available(struct pl_data *chip)
 	return true;
 }
 
+#ifdef CONFIG_MACH_XIAOMI_SDM660
+#define PARALLEL_FLOAT_VOLTAGE_DELTA_UV 100000
+#else
 #define PARALLEL_FLOAT_VOLTAGE_DELTA_UV 50000
+#endif
 static int pl_fv_vote_callback(struct votable *votable, void *data,
 			int fv_uv, const char *client)
 {
@@ -1269,6 +1273,11 @@ static int pl_fv_vote_callback(struct votable *votable, void *data,
 
 #define ICL_STEP_UA	25000
 #define PL_DELAY_MS     3000
+#ifdef CONFIG_MACH_XIAOMI_SDM660
+#define ICL_MAX_UA	1300000
+#else
+#define ICL_MAX_UA	1400000
+#endif
 static int usb_icl_vote_callback(struct votable *votable, void *data,
 			int icl_ua, const char *client)
 {
@@ -1291,7 +1300,7 @@ static int usb_icl_vote_callback(struct votable *votable, void *data,
 	vote(chip->pl_disable_votable, ICL_CHANGE_VOTER, true, 0);
 
 	/*
-	 * if (ICL < 1400)
+	 * if (ICL < ICL_MAX_UA)
 	 *	disable parallel charger using USBIN_I_VOTER
 	 * else
 	 *	instead of re-enabling here rely on status_changed_work
@@ -1299,7 +1308,7 @@ static int usb_icl_vote_callback(struct votable *votable, void *data,
 	 *	unvote USBIN_I_VOTER) the status_changed_work enables
 	 *	USBIN_I_VOTER based on settled current.
 	 */
-	if (icl_ua <= 1400000)
+	if (icl_ua <= ICL_MAX_UA)
 		vote(chip->pl_enable_votable_indirect, USBIN_I_VOTER, false, 0);
 	else
 		schedule_delayed_work(&chip->status_change_work,
@@ -1769,10 +1778,10 @@ static void handle_settled_icl_change(struct pl_data *chip)
 	}
 	main_limited = pval.intval;
 
-	if ((main_limited && (main_settled_ua + chip->pl_settled_ua) < 1400000)
+	if ((main_limited && (main_settled_ua + chip->pl_settled_ua) < ICL_MAX_UA)
 			|| (main_settled_ua == 0)
 			|| ((total_current_ua >= 0) &&
-				(total_current_ua <= 1400000)))
+				(total_current_ua <= ICL_MAX_UA)))
 		vote(chip->pl_enable_votable_indirect, USBIN_I_VOTER, false, 0);
 	else
 		vote(chip->pl_enable_votable_indirect, USBIN_I_VOTER, true, 0);

drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -615,6 +615,39 @@ static int fg_get_battery_temp(struct fg_dev *fg, int *val)
 
 	/* Value is in Kelvin; Convert it to deciDegC */
 	temp = (temp - 273) * 10;
+#ifdef CONFIG_MACH_XIAOMI_SDM660
+	if (temp < -80) {
+		switch (temp) {
+			case -90:
+				temp = -110;
+				break;
+			case -100:
+				temp = -120;
+				break;
+			case -110:
+				temp = -130;
+				break;
+			case -120:
+				temp = -150;
+				break;
+			case -130:
+				temp = -170;
+				break;
+			case -140:
+				temp = -190;
+				break;
+			case -150:
+				temp = -200;
+				break;
+			case -160:
+				temp = -210;
+				break;
+			default:
+				temp -= 50;
+				break;
+		};
+	}
+#endif
 	*val = temp;
 	return 0;
 }
@@ -1854,10 +1887,17 @@ static int fg_adjust_recharge_voltage(struct fg_dev *fg)
 
 	recharge_volt_mv = chip->dt.recharge_volt_thr_mv;
 
+#ifdef CONFIG_MACH_XIAOMI_SDM660
+	if (fg->health == POWER_SUPPLY_HEALTH_WARM)
+		recharge_volt_mv = 4050;
+	if (fg->health == POWER_SUPPLY_HEALTH_COOL)
+		recharge_volt_mv = 4280;
+#else
 	/* Lower the recharge voltage in soft JEITA */
 	if (fg->health == POWER_SUPPLY_HEALTH_WARM ||
 			fg->health == POWER_SUPPLY_HEALTH_COOL)
 		recharge_volt_mv -= 200;
+#endif
 
 	rc = fg_set_recharge_voltage(fg, recharge_volt_mv);
 	if (rc < 0) {
@@ -4182,6 +4222,9 @@ static int fg_hw_init(struct fg_dev *fg)
 	if (chip->dt.delta_soc_thr > 0 && chip->dt.delta_soc_thr < 100) {
 		fg_encode(fg->sp, FG_SRAM_DELTA_MSOC_THR,
 			chip->dt.delta_soc_thr, buf);
+#ifdef CONFIG_MACH_XIAOMI_SDM660
+		buf[0] = 0x8;
+#endif
 		rc = fg_sram_write(fg,
 				fg->sp[FG_SRAM_DELTA_MSOC_THR].addr_word,
 				fg->sp[FG_SRAM_DELTA_MSOC_THR].addr_byte,
@@ -4416,6 +4459,14 @@ static int fg_hw_init(struct fg_dev *fg)
 			return rc;
 		}
 	}
+
+#ifdef CONFIG_MACH_XIAOMI_SDM660
+	buf[0] = 0x33;
+	buf[1] = 0x3;
+	rc = fg_sram_write(fg, 4, 0, buf, 2, FG_IMA_DEFAULT);
+	if (rc < 0)
+		pr_err("Error in configuring Sram,rc = %d\n",rc);
+#endif
 
 	return 0;
 }
@@ -5076,7 +5127,11 @@ static int fg_parse_dt(struct fg_gen3_chip *chip)
 	if (rc < 0)
 		chip->dt.sys_term_curr_ma = DEFAULT_SYS_TERM_CURR_MA;
 	else
+#ifdef CONFIG_MACH_XIAOMI_SDM660
+		chip->dt.sys_term_curr_ma = -temp;
+#else
 		chip->dt.sys_term_curr_ma = temp;
+#endif
 
 	rc = of_property_read_u32(node, "qcom,fg-chg-term-base-current", &temp);
 	if (rc < 0)

drivers/power/supply/qcom/qpnp-smb2.c
@@ -1152,7 +1152,11 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 					      FG_ESR_VOTER);
 		break;
 	case POWER_SUPPLY_PROP_TECHNOLOGY:
+#ifdef CONFIG_MACH_XIAOMI_SDM660
+		val->intval = POWER_SUPPLY_TECHNOLOGY_LIPO;
+#else
 		val->intval = POWER_SUPPLY_TECHNOLOGY_LION;
+#endif
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_DONE:
 		rc = smblib_get_prop_batt_charge_done(chg, val);
@@ -1184,12 +1188,16 @@ static int smb2_batt_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_VOLTAGE_NOW:
 	case POWER_SUPPLY_PROP_TEMP:
 	case POWER_SUPPLY_PROP_TIME_TO_FULL_NOW:
+#ifndef CONFIG_MACH_XIAOMI_SDM660
 		rc = smblib_get_prop_from_bms(chg, psp, val);
 		break;
+#endif
 	case POWER_SUPPLY_PROP_CURRENT_NOW:
 		rc = smblib_get_prop_from_bms(chg, psp, val);
+#ifndef CONFIG_MACH_XIAOMI_SDM660
 		if (!rc)
 			val->intval *= (-1);
+#endif
 		break;
 	case POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE:
 		val->intval = chg->fcc_stepper_enable;
@@ -1711,6 +1719,23 @@ static int smb2_init_hw(struct smb2 *chip)
 			true, 0);
 	vote(chg->pd_disallowed_votable_indirect, PD_NOT_SUPPORTED_VOTER,
 			chip->dt.no_pd, 0);
+
+#ifdef CONFIG_MACH_XIAOMI_SDM660
+	/* Operate the QC2.0 in 5V/9V mode i.e. Disable 12V */
+	rc = smblib_masked_write(chg, HVDCP_PULSE_COUNT_MAX_REG,
+								PULSE_COUNT_QC2P0_12V | PULSE_COUNT_QC2P0_9V, PULSE_COUNT_QC2P0_9V);
+	if (rc < 0) {
+		dev_err(chg->dev, "Couldn't configure QC2.0 to 9V rc=%d\n", rc);
+		return rc;
+	}
+	/* Operate the QC3.0 to limit vbus to 6.6v*/
+	rc = smblib_masked_write(chg, HVDCP_PULSE_COUNT_MAX_REG, PULSE_COUNT_QC3P0_MASK, 0x8);
+	if (rc < 0) {
+		dev_err(chg->dev, "Couldn't configure QC3.0 to 6.6V rc=%d\n", rc);
+		return rc;
+	}
+#endif
+
 	/*
 	 * AICL configuration:
 	 * start from min and AICL ADC disable
@@ -1876,6 +1901,10 @@ static int smb2_init_hw(struct smb2 *chip)
 		return rc;
 	}
 
+#ifdef CONFIG_MACH_XIAOMI_SDM660
+	rc = vote(chg->chg_disable_votable, DEFAULT_VOTER, true, 0);
+#endif
+
 	switch (chip->dt.chg_inhibit_thr_mv) {
 	case 50:
 		rc = smblib_masked_write(chg, CHARGE_INHIBIT_THRESHOLD_CFG_REG,
@@ -1903,6 +1932,10 @@ static int smb2_init_hw(struct smb2 *chip)
 	default:
 		break;
 	}
+
+#ifdef CONFIG_MACH_XIAOMI_SDM660
+	rc = vote(chg->chg_disable_votable, DEFAULT_VOTER, false, 0);
+#endif
 
 	if (rc < 0) {
 		dev_err(chg->dev, "Couldn't configure charge inhibit threshold rc=%d\n",

drivers/power/supply/qcom/smb1351-charger.c
@@ -533,6 +533,10 @@ static int pre_chg_current[] = {
 	200, 300, 400, 500, 600, 700,
 };
 
+#ifdef CONFIG_MACH_XIAOMI_SDM660
+bool is_global_version = false;
+#endif
+
 static int smb1351_read_reg(struct smb1351_charger *chip, int reg, u8 *val)
 {
 	s32 ret;
@@ -734,7 +738,9 @@ static int smb1351_fastchg_current_set(struct smb1351_charger *chip,
 		(fastchg_current > SMB1351_CHG_FAST_MAX_MA)) {
 		pr_err("bad pre_fastchg current mA=%d asked to set\n",
 					fastchg_current);
+#ifndef CONFIG_MACH_XIAOMI_SDM660
 		return -EINVAL;
+#endif
 	}
 
 	/*
@@ -1567,7 +1573,9 @@ static int smb1351_parallel_set_chg_suspend(struct smb1351_charger *chip,
 	if (chip->parallel_charger_suspended == suspend) {
 		pr_debug("Skip same state request suspended = %d suspend=%d\n",
 				chip->parallel_charger_suspended, !suspend);
+#ifndef CONFIG_MACH_XIAOMI_SDM660
 		return 0;
+#endif
 	}
 
 	if (!suspend) {
@@ -1661,6 +1669,18 @@ static int smb1351_parallel_set_chg_suspend(struct smb1351_charger *chip,
 		}
 		chip->parallel_charger_suspended = false;
 	} else {
+#ifdef CONFIG_MACH_XIAOMI_SDM660
+		smb1351_enable_volatile_writes(chip);
+		/* control USB suspend via command bits */
+		rc = smb1351_masked_write(chip, VARIOUS_FUNC_REG,
+					APSD_EN_BIT | SUSPEND_MODE_CTRL_BIT,
+						SUSPEND_MODE_CTRL_BY_I2C);
+		if (rc) {
+			pr_err("Couldn't set USB suspend rc=%d\n", rc);
+			return rc;
+		}
+#endif
+
 		rc = smb1351_usb_suspend(chip, CURRENT, true);
 		if (rc)
 			pr_debug("failed to suspend rc=%d\n", rc);
@@ -2727,6 +2747,16 @@ static int is_parallel_charger(struct i2c_client *client)
 	return of_property_read_bool(node, "qcom,parallel-charger");
 }
 
+#ifdef CONFIG_MACH_XIAOMI_SDM660
+static int __init hwc_setup(char *s)
+{
+	is_global_version = strcmp(s, "Global") != 0;
+	return 1;
+}
+
+__setup("androidboot.hwc=", hwc_setup);
+#endif
+
 static int create_debugfs_entries(struct smb1351_charger *chip)
 {
 	struct dentry *ent;
@@ -2944,6 +2974,12 @@ static int smb1351_parallel_charger_probe(struct i2c_client *client,
 	struct device_node *node = client->dev.of_node;
 	struct power_supply_config parallel_psy_cfg = {};
 
+#ifdef CONFIG_MACH_XIAOMI_SDM660
+	if (is_global_version) {
+		return -ENODEV;
+	}
+#endif
+
 	chip = devm_kzalloc(&client->dev, sizeof(*chip), GFP_KERNEL);
 	if (!chip)
 		return -ENOMEM;
@@ -3117,7 +3153,29 @@ static struct i2c_driver smb1351_charger_driver = {
 	.id_table	= smb1351_charger_id,
 };
 
+#ifdef CONFIG_MACH_XIAOMI_SDM660
+static int __init smb1351_charger_init(void)
+{
+	struct power_supply *pl_psy = power_supply_get_by_name("parallel");
+
+	if (pl_psy) {
+		pr_info("Another parallel driver has been registered\n");
+		return -ENOENT;
+	}
+
+	return i2c_add_driver(&smb1351_charger_driver);
+}
+
+static void __exit smb1351_charger_exit(void)
+{
+	i2c_del_driver(&smb1351_charger_driver);
+}
+
+late_initcall(smb1351_charger_init);
+module_exit(smb1351_charger_exit);
+#else
 module_i2c_driver(smb1351_charger_driver);
+#endif
 
 MODULE_DESCRIPTION("smb1351 Charger");
 MODULE_LICENSE("GPL v2");